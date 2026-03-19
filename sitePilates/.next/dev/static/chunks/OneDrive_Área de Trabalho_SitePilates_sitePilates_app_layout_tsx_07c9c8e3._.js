543de3974ed39bebc527facc5193b7fb210377e1
= <=>
2026-03-19 14:32:49 -0300

    delete

--- sitePilates/.next/dev/static/chunks/OneDrive_Área de Trabalho_SitePilates_sitePilates_app_layout_tsx_07c9c8e3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__4994f0bf._.css",
-  "static/chunks/a1f8a_f86b3406._.js"
-],
-    source: "dynamic"
-});
